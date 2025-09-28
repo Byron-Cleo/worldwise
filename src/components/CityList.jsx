7fa16321a5230732a52d1d7798adf1d37970cea8
Byron Ochara <byron.ochara@gmail.com>
2025-09-29 01:36:23 +0300

    created city list component

---
+++ src/components/CityList.jsx
@@ -0,0 +1,11 @@
+import styles from "./CityList.module.css";
+
+function CityList() {
+  return (
+    <ul className={styles.cityList}>
+      <p>LIST</p>
+    </ul>
+  );
+}
+
+export default CityList;
