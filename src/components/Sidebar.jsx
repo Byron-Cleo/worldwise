079ed2f7b99912fdd80d1d6ca4b86ba527038c8e
Byron Ochara <byron.ochara@gmail.com>
2025-09-29 01:18:03 +0300

    updated sidebar to contain the nested links with the Outlet component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import AppNav from "./AppNav";
 import Logo from "./Logo";
 import styles from "./Sidebar.module.css";
@@ -5,9 +6,11 @@ import styles from "./Sidebar.module.css";
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
-      <AppNav />
       <Logo />
-      <p>List of cities</p>
+      <AppNav />
+      
+      <Outlet />
+
       <footer className={styles.footer}>
         <p className={styles.copyright}>
             &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
