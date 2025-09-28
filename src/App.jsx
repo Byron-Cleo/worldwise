f4aee3f6536d66e4f59b307f5762cd67aad09a01
Byron Ochara <byron.ochara@gmail.com>
2025-09-28 21:27:02 +0300

    created application mark-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+import Product from "./pages/Product";
+
 function App() {
   return (
-    <div>
-      Application has started now
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
