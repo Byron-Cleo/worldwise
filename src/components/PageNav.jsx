27e865a75f997f80b8b0edd3adf3c1e9d7e6694b
Byron Ochara <byron.ochara@gmail.com>
2025-09-28 22:01:11 +0300

    added login navigation

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -2,26 +2,24 @@ import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
 import styles from "./PageNav.module.css";
 
-
 function PageNav() {
-    return (
-        <nav className={styles.nav}>
-            <Logo />
-            
-            <ul>
-                <li>
-                    <NavLink to="/" activeClassName="active" exact>Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/pricing" activeClassName="active" exact>Pricing</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/product" activeClassName="active" exact>Product</NavLink>
-                </li>
-            </ul>
-            
-        </nav>
-    )
+  return (
+    <nav className={styles.nav}>
+      <Logo />
+
+      <ul>
+        <li>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default PageNav
+export default PageNav;
