3064159d8193fc54c5215cc5ccff7f54e8ce4843
Byron Ochara <byron.ochara@gmail.com>
2025-09-29 11:45:07 +0300

    updated code for fetching cities from a resource

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
@@ -7,7 +8,28 @@ import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import PageNotFound from "./pages/PageNotFound";
 
+const BASE_URL = "http://localhost:9000";
+
 function App() {
+  const [cities, setCites] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCites(data);
+      } catch (err) {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -17,15 +39,18 @@ function App() {
         <Route path="/product" element={<Product />} />
         <Route path="/login" element={<Login />} />
         <Route path="/app" element={<AppLayout />}>
-          <Route index element={<CityList />} />
-          <Route path="/app/cities" element={<CityList />} />
-          <Route path="/app/countries" element={<p>List of countries here</p>} />
+          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="/app/cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route
+            path="/app/countries"
+            element={<p>List of countries here</p>}
+          />
           <Route path="/app/form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
