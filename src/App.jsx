eb686806363b19a0bcc12fa63db35e7a865e11a8
Byron Ochara <byron.ochara@gmail.com>
2025-09-29 07:37:38 +0300

    updated file for nested links

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
 import PageNotFound from "./pages/PageNotFound";
 
 function App() {
@@ -13,10 +14,11 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/product" element={<Product />} />
+        <Route path="/product" element={<Product />} />
         <Route path="/login" element={<Login />} />
         <Route path="/app" element={<AppLayout />}>
-          <Route index element={<p>LIST</p>} />
-          <Route path="/app/cities" element={<p>List of cities here</p>} />
+          <Route index element={<CityList />} />
+          <Route path="/app/cities" element={<CityList />} />
           <Route path="/app/countries" element={<p>List of countries here</p>} />
           <Route path="/app/form" element={<p>Form</p>} />
         </Route>
