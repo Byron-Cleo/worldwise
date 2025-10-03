6fa1b962de91d29b2fef9605133754e1029cffd0
Byron Ochara <byron.ochara@gmail.com>
2025-10-03 20:42:56 +0300

    implementing useSearchParams hook

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,17 @@
+import { useSearchParams, useNavigate } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
+  const navigate = useNavigate();
+
+  const [searchParams, ] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  console.log("LAT LNG", lat, lng);
+
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate("/app/form")}>
       <p>Map side here</p>
     </div>
   );
