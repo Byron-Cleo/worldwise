9ba617aaf30603ba72e1fb33b32ef505d8cf6b74
Byron Ochara <byron.ochara@gmail.com>
2025-10-08 00:14:34 +0300

    created cities functon to update visited cities in the database server

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -27,20 +27,39 @@ function CitiesProvider({ children }) {
 
   async function getCity(id) {
     try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities/${id}`);
-        const data = await res.json();
-        setCurrentCity(data);
-      } catch (err) {
-        alert("There was an error loading data...");
-      } finally {
-        setIsLoading(false);
-      }
-
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (err) {
+      alert("There was an error loading data...");
+    } finally {
+      setIsLoading(false);
+    }
   }
 
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: "POST",
+        body: JSON.stringify(newCity),
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+      console.log("DDDD", data);
+      // setCurrentCity(data);
+      setCites(cities => [...cities, data])
+    } catch (err) {
+      alert("There was an error loading data...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
   return (
-    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
+    <CitiesContext.Provider
+      value={{ cities, isLoading, currentCity, getCity, createCity }}
+    >
       {children}
     </CitiesContext.Provider>
   );
